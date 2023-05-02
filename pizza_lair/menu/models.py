620e451f4f19b67b6a2639601ba23749b9da2dc3
LovisaBa <103430711+LovisaBa@users.noreply.github.com>
2023-05-02 13:51:17 +0000

    menu and main models

--- pizza_lair/menu/models.py
+++ pizza_lair/menu/models.py
@@ -1,17 +1,35 @@
 from django.db import models
+from main.models import Products
 
 # Create your models here.
 
-"""class PizzaType(models.Model):
-    name = models.CharField(max_length=255)
-class Pizza(models.Model):
-    # add relevant attributes for pizzas.
-    name = models.CharField(max_length=255)
-    type = models.ForeignKey(PizzaType, on_delete=models.CASCADE)
-    price = models.FloatField()
-    description = models.CharField(max_length=255, blank=True)
-    toppings = models #??
+class PizzaTypes(models.Model):
+    type = models.CharField(max_length=255)
 
-class PizzaImage(models.Model):
-    menu = models.ForeignKey(Pizza, on_delete=models.CASCADE)
-    image = models.CharField(max_length=9999)"""
+class Pizzas(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    type = models.ForeignKey(PizzaTypes, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    description = models.CharField(max_length=255, blank=True)
+    image = models.CharField(max_length=9999)
+
+class PizzaHasType(models.Model):
+    pizza = models.ForeignKey(Pizzas, on_delete=models.CASCADE)
+    type = models.ForeignKey(PizzaTypes, on_delete=models.CASCADE)
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=255)
+
+class PizzaIngredients(models.Model):
+    pizza = models.ForeignKey(Pizzas, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
