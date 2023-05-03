2b1b633ddd9d8d1cb268f959339bf002535b55d3
LovisaBa <103430711+LovisaBa@users.noreply.github.com>
2023-05-03 16:44:54 +0000

    Pizza images display on site :pizza:

--- pizza_lair/pizza_lair/urls.py
+++ pizza_lair/pizza_lair/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 '''The only route currently is localhost:3000/admin/'''
 
@@ -27,3 +29,8 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('orders/', include('orders.urls'))
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
