b8f1d4593c3aa9ed555040b1f1d06e2c4dd11e94
LovisaBa <103430711+LovisaBa@users.noreply.github.com>
2023-05-05 13:24:34 +0000

    Types added to Product and Offer models

--- pizza_lair/main/models.py
+++ pizza_lair/main/models.py
@@ -3,6 +3,15 @@ from django.db import models
 # Create your models here.
 
 
+class ProdType(models.Model):
+    type = models.TextField(max_length=255)
+
+    def __str__(self):
+        return f"{self.type}"
+
+
 class Product(models.Model):
+    type = models.ForeignKey(ProdType, on_delete=models.CASCADE)
+
     def __str__(self):
         return f"ProductNr: {self.id}"
