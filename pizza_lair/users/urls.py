bfa9042e7fe0dbe437a33b16aff4d82cdb3d4adf
estherir <112634267+estherir@users.noreply.github.com>
2023-05-08 09:21:24 +0000

    User profile image visible when logged in

--- pizza_lair/users/urls.py
+++ pizza_lair/users/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # http://localhost:8000/pizzas
     path('create_user', views.create_user, name="create_user"),
     path('login', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout', LogoutView.as_view(next_page='/'), name='logout'),
     path('profile', views.profile, name='profile', )
 ]
