5ffdf53bed51a4674161ef31d7495806bfafc7fd
LovisaBa <103430711+LovisaBa@users.noreply.github.com>
2023-05-10 11:30:08 +0000

    Offer model updated etc..

--- pizza_lair/offers/views.py
+++ pizza_lair/offers/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+
+from menu.models import Pizza
 from offers.models import PizzaOffer, Discount
+
 
 # Create your views here.
 
@@ -11,13 +14,33 @@ def index(request):
     })
 
 
-def get_offer_by_id(request, id):
+def get_offer(request, id) -> PizzaOffer:
+    print(request.POST)
     return render(request, 'offers/offer_details.html', {
-        'offer': get_object_or_404(PizzaOffer, pk=id)
+        'offer': get_object_or_404(PizzaOffer, pk=id),
+        #"pizzas"
     })
 
 
-def get_discount_by_id(request, id):
+def get_discount(request, id) -> Discount:
     return render(request, 'offers/discount_details.html', {
         'discount': get_object_or_404(Discount, pk=id)
     })
+
+
+def get_offer_by_id(offer_id):
+    return get_object_or_404(PizzaOffer, pk=offer_id)
+
+
+def get_pizzas(request, amount):
+    pizzas = []
+    return pizzas
+
+
+def add_pizzas_to_offer(request, offer_id):
+    offer = get_offer_by_id(offer_id)
+    amount = offer.pizza_amount
+    pizzas = get_pizzas(request, amount)
+    for pizza in pizzas:
+        pizza.price = round(offer.price/offer.pizza_amount)
+    return pizzas
