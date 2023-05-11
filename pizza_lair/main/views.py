684cae568fba5b1a7da04cf16b5bc0c1ff472db4
LovisaBa <103430711+LovisaBa@users.noreply.github.com>
2023-05-11 11:35:59 +0000

    docstring added

--- pizza_lair/main/views.py
+++ pizza_lair/main/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 
 def index(request):
+    """Renders the main site of the webpage."""
     return render(request, 'main/index.html',)
 
+
 def about(request):
+    """Renders the 'about us' site."""
     return render(request, 'main/about_us.html')
